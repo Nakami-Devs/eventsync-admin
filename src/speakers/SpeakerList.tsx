b2b463cd343e16771dc9655c4363f311dc7db733
nelio-gio <hei.rocques@gmail.com>
2026-02-19 21:44:57 +0300

    feat: add edit buttons to speaker list

--- src/speakers/SpeakerList.tsx
+++ src/speakers/SpeakerList.tsx
@@ -1,19 +1,23 @@
 import {
   List, Datagrid, TextField,
-  UrlField, ImageField, DeleteWithConfirmButton
+  UrlField, ImageField,
+  EditButton, DeleteWithConfirmButton
 } from 'react-admin'
 
 export const SpeakerList = () => (
   <List>
-    <Datagrid rowClick="edit">
+    <Datagrid>
       <ImageField
         source="profile_pic"
         label="Photo"
         sx={{ '& img': { width: 50, height: 50, borderRadius: '50%', objectFit: 'cover' } }}
       />
-      <TextField source="full_name"       label="Nom complet"    />
-      <TextField source="biography"       label="Biographie"     />
-      <UrlField  source="external_links"  label="Liens externes" />
+      <TextField source="full_name"      label="Nom complet"    />
+      <TextField source="biography"      label="Biographie"     />
+      <UrlField  source="external_links" label="Liens externes" />
+
+      <EditButton label="Modifier" />
+
       <DeleteWithConfirmButton
         confirmTitle="Supprimer cet intervenant ?"
         confirmContent="Cette action est irréversible."
