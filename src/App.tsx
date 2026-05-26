11cdfc39031985d9bde5079a3e8e5e5054cf9184
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-05-26 10:49:29 +0300

    feat: add Admin and Resource properties inside App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
-import {Dashboard} from "./dashboard/Dashboard.tsx";
+import {Dashboard} from "./components/dashboard/Dashboard.tsx";
 import {Admin, Resource} from "react-admin";
 import {authProvider} from "./authProvider.ts";
 import {dataProvider} from "./dataProvider.ts";
+import { AdminLayout } from './components/layout/Layout.tsx';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
           authProvider={authProvider}
           dataProvider={dataProvider}
           dashboard={Dashboard}
+          layout={AdminLayout}
       >
           <Resource
               name="events"
