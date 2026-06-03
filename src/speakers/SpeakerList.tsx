1ef331e29e32cefcc2a7bf54fc5271e52688e3e0
nelio-gio <hei.rocques@gmail.com>
2026-06-03 09:37:55 +0300

    fix: resolve delete botton

--- src/speakers/SpeakerList.tsx
+++ src/speakers/SpeakerList.tsx
@@ -1,12 +1,24 @@
-import { List, Datagrid, TextField, UrlField, ImageField } from 'react-admin'
+import {
+  List, Datagrid, TextField,
+  UrlField, ImageField, DeleteWithConfirmButton
+} from 'react-admin'
 
 export const SpeakerList = () => (
   <List>
     <Datagrid rowClick="edit">
-      <ImageField source="profile_pic"   label="Photo"           sx={{ '& img': { width: 50, height: 50, borderRadius: '50%', objectFit: 'cover' } }} />
-      <TextField  source="full_name"     label="Nom complet"     />
-      <TextField  source="biography"     label="Biographie"      />
-      <UrlField   source="external_links" label="Liens externes" />
+      <ImageField
+        source="profile_pic"
+        label="Photo"
+        sx={{ '& img': { width: 50, height: 50, borderRadius: '50%', objectFit: 'cover' } }}
+      />
+      <TextField source="full_name"       label="Nom complet"    />
+      <TextField source="biography"       label="Biographie"     />
+      <UrlField  source="external_links"  label="Liens externes" />
+      <DeleteWithConfirmButton
+        confirmTitle="Supprimer cet intervenant ?"
+        confirmContent="Cette action est irréversible."
+        onClick={(e) => e.stopPropagation()}
+      />
     </Datagrid>
   </List>
 )
