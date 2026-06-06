c1b6d71071dab605188cb580c387c193ee35085f
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-07 01:45:38 +0300

    feat: install MUI and create dashboard

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,14 +1,53 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import {Container, Stack, Grid, Typography} from '@mui/material';
+import {useGetList} from "react-admin";
+import {StatCard} from "../StatCard.tsx";
 
-export const Dashboard = () => (
-    <Card>
-        <CardContent>
-            <Typography variant="h5" gutterBottom>
-                Bienvenue sur EventSync Admin
-            </Typography>
-            <Typography variant="body2">
-                Gérez vos événements, intervenants et salles en tant réel depuis ce plateforme.
-            </Typography>
-        </CardContent>
-    </Card>
-);
+export const Dashboard = () => {
+    const {total: totalEvents, isPending: p1} = useGetList(
+        "events",
+        {
+            pagination:
+                {page: 1, perPage: 1}
+        }
+    )
+
+    const {total: totalSessions, isPending: p3} = useGetList(
+        "sessions",
+        {
+            pagination:
+                {page: 1, perPage: 1}
+        }
+    )
+
+    const {total: totalSpeakers, isPending: p2} = useGetList(
+        "speakers",
+        {
+            pagination:
+                {page: 1, perPage: 1}
+        }
+    )
+
+    return (
+        <Container maxWidth="lg" sx={{mt: 24, mb: 4}}>
+            <Stack spacing={3}>
+                <Stack spacing={1}>
+                    <Typography variant="h2" color="#7C3AED" fontWeight="bold">EventSync, vivez votre événement autrement</Typography>
+                    <Typography variant="body1">Naviguez dans le planning, identifiez les sessions en cours et
+                        interagissez
+                        avec les intervenants en temps réel.</Typography>
+                </Stack>
+                <Grid container spacing={3}>
+                    <Grid size={{xs: 12, sm: 6, md: 4}}>
+                        <StatCard title="Événements" value={totalEvents} isPending={p1}/>
+                    </Grid>
+                    <Grid size={{xs: 12, sm: 6, md: 4}}>
+                        <StatCard title="Intervenants" value={totalSpeakers} isPending={p2}/>
+                    </Grid>
+                    <Grid size={{xs: 12, sm: 6, md: 4}}>
+                        <StatCard title="Sessions" value={totalSessions} isPending={p3}/>
+                    </Grid>
+                </Grid>
+            </Stack>
+        </Container>
+    )
+}
