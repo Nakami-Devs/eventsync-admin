9f718f85df9cab76c00aad52910395af28ff4182
Fanamby <hei.fanamby@gmail.com>
2026-06-08 20:18:44 +0300

    feat: add index.ts

---
+++ src/rooms/index.ts
@@ -0,0 +1,12 @@
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+
+import { RoomsList } from "./RoomsList";
+import { RoomCreate } from "./RoomCreate";
+import { RoomEdit } from "./RoomEdit";
+
+export default {
+    list: RoomsList,
+    create: RoomCreate,
+    edit: RoomEdit,
+    icon: MeetingRoomIcon
+};
