7a3224a496f4e75629907a339ddbdeeabf5b749c
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-28 16:05:09 +0300

    fix(events): add validation to EventCreate fields and redirection to list

--- src/resources/events/EventCreate.tsx
+++ src/resources/events/EventCreate.tsx
@@ -3,16 +3,17 @@ import {
   SimpleForm,
   TextInput,
   DateTimeInput,
+  required,
 } from "react-admin";
 
 export const EventCreate = () => (
-  <Create>
+  <Create redirect="list">
     <SimpleForm>
-      <TextInput label="Titre" source="title" />
-      <TextInput label="Description" source="description" multiline />
-      <DateTimeInput label="Date de debut" source="start_date" />
-      <DateTimeInput label="Date de fin" source="end_date" />
-      <TextInput label="Place" source="place" />
+      <TextInput label="Titre" source="title" validate={required()} />
+      <TextInput label="Description" source="description" multiline validate={required()}/>
+      <DateTimeInput label="Date de debut" source="start_date" validate={required()}/>
+      <DateTimeInput label="Date de fin" source="end_date" validate={required()}/>
+      <TextInput label="Place" source="place" validate={required()}/>
     </SimpleForm>
   </Create>
 );
